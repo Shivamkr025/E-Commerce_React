6fb2b77c2cbf4478c70fab005154fef94d6603a0
Shivamkr025 <shivam22@navgurukul.org>
2025-01-06 21:10:03 +0530

    update server file

--- Backend/app.js
+++ Backend/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import RouterFile from './router/shopRouter.js';
+import cookieParser from 'cookie-parser';
+import RouterFile from './router/userRouter.js';
 
 const app = express()
 const PORT = 6052
 
 app.use(express.json())
+app.use(cookieParser())
 app.use('/', RouterFile)
 
 mongoose.connect('mongodb://localhost:27017/E-Commerce_Project')
