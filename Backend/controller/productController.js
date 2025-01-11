b18fab14a18832f27b3490544b4cd8f9aebd331d
Shivamkr025 <shivam22@navgurukul.org>
2025-01-11 21:57:42 +0530

    add and update all file

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -40,3 +40,61 @@ export const addProduct = async (req, res) => {
         res.status(500).json({ error: 'Something went wrong while adding the product.' });
     }
 };
+export const getAllProduct = async(req, res) => {
+    try {
+        const fetchProduct = await Product.find()
+        res.status(200).json({message:"successfully get all product", fetchProduct})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Someting went wrong in get all product function"})
+        
+    }
+}
+
+export const getSingleProduct = async(req, res) => {
+    try {
+        const {id} = req.params
+        const product = await Product.findById(id)
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json({ success: true, product });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Failed to fetch product details", error });
+    }
+}
+
+export const updateProduct = async(req, res) => {
+    const {id} = req.body
+    try {
+        const updateProdct = await Product.findById(id)
+        if(!updateProdct){
+            return res.status(400).json({message: "Invalid Product Please Check Your id "})
+        }
+
+        const submit = await Product.findByIdAndUpdate({id}, {$set:{...req.body}})
+        res.status(200).json({product: submit})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: "Something went wrong in update product fuction"})
+        
+    }
+}
+
+export const deleteProduct = async(req, res) => {
+
+}
+
+export const productRating = async(req, res) => {
+
+}
+
+export const searchProduct = async(req, res) => {
+
+}
+
+export const getProductByCategory = async(req, res) => {
+    
+}
--- Backend/router/ProductRouter.js
+++ Backend/router/ProductRouter.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import upload from '../middleware/uploadMiddleware.js';
-import { addProduct } from '../controller/productController.js';
+import { addProduct, getAllProduct, getSingleProduct, updateProduct } from '../controller/productController.js';
 
 const router = express.Router()
 
 router.post('/add/product', upload.single('images'), addProduct)
+
+router.get('/get/all-product', getAllProduct)
+
+router.get('/products/:id', getSingleProduct)
+
+router.post('/update/product/:id', updateProduct)
 
 export default router
